feaf80b1aa4a934ab1fec5b0cf43e8f7ac409dc8
Heera Hemanth <heerahemanth1@gmail.com>
2023-08-29 21:22:29 +0530

    Article component fully fledged with Global navigation
    Updated the article component to display the navigation at the top with the global navigation component.
    Added a global navigation component that can be re-used in all the other components.
    It is slightly different from the homepage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
+import { GlobalNavComponent } from './global-nav/global-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ArticleComponent
+    ArticleComponent,
+    GlobalNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/global-nav/global-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-nav',
+  templateUrl: './global-nav.component.html',
+  styleUrls: ['./global-nav.component.scss']
+})
+export class GlobalNavComponent {
+
+}
