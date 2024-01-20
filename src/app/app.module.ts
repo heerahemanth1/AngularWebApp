419864d75fb2e08082474381ac5b6b9adeaab7b3
Heera Hemanth <heerahemanth1@gmail.com>
2024-01-20 18:48:01 +0530

    We have added the --auth-- and --about-- components. Only boilerplate code added. Registered the components in App module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
 import { GlobalNavComponent } from './global-nav/global-nav.component';
+import { AuthComponent } from './auth/auth.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ArticleComponent,
-    GlobalNavComponent
+    GlobalNavComponent,
+    AuthComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
